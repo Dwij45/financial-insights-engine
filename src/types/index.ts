3730f9f20137778d11b2d70a7de0c897cf07f0df
Dwij <gosaidwij045@gmail.com>
2026-04-04 05:15:24 +0530

    feat : Add Register and login types

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+import type { Request } from 'express'
+import { Types } from 'mongoose'
+
+export type Role = 'viewer' | 'analyst' | 'admin'
+
+//the inputs
+export interface RegisterDto {
+    name : string
+    email : string
+    password : string
+    role : Role
+}
+export interface LoginDto {
+  email: string
+  password: string
+}
+export interface CreateTransactionDto {
+    
+}
